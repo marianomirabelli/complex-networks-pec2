d5034fc0277af6b7aef02cb169c3c37c40c35323
Mariano <mariano2106@gmail.com>
2019-04-13 01:32:51 -0300

    feature: add histogram for theoretical probability

--- sources/ERModel/ERAlgorithm.py
+++ sources/ERModel/ERAlgorithm.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import random
+import collections
+import math
+from scipy.special import comb
 
 
 def draw_graph(g):
-    layout = nx.circular_layout(g)
+    layout = nx.random_layout(g)
     nx.draw_networkx(g, layout, False, False)
     plt.show()
 
@@ -25,13 +28,31 @@ def generate_erdos_renyi(g,p):
                 if(r<=p and (not g.has_edge(j,i))):
                     g.add_edge(i,j)
 
+def draw_bar(degrees, probabilites):
+    plt.bar(degrees, probabilites, width=0.80, color='b')
+    plt.gca().set_xlabel("Degree")
+    plt.gca().set_ylabel("Binomial P(K)")
+    plt.show()
+
+
+def draw_theoretical_degree_distribution(g,p):
+    degree_sequence = sorted([d for n, d in g.degree()], reverse=True)
+    n = len(g)
+    probabilities_list = list()
+    for degree in degree_sequence:
+        probabilities_list.append(comb(n-1,degree)
+                                  *(math.pow(p,degree))
+                                  *(math.pow(1-p,(n-1-degree))))
+    draw_bar(degree_sequence,probabilities_list)
+
 
 def main():
-    n = int(raw_input('Enter the number of nodes'))
-    p = float(raw_input('Enter the probability'))
-    disconnected_graph = create_graph(n)
-    generate_erdos_renyi(disconnected_graph, p)
-    draw_graph(disconnected_graph)
+    n = int(raw_input('Enter the number of nodes '))
+    p = float(raw_input('Enter the probability '))
+    graph = create_graph(n)
+    generate_erdos_renyi(graph, p)
+    draw_graph(graph)
+    draw_theoretical_degree_distribution(graph,p)
 
 
 main()
