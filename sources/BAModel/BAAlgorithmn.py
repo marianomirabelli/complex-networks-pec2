a5247390cbba9816f2f54dc73409c558f84225e6
Mariano <mariano2106@gmail.com>
2019-04-20 10:40:08 -0300

    feature: add log empirical calculation

--- sources/BAModel/BAAlgorithmn.py
+++ sources/BAModel/BAAlgorithmn.py
@@ -1,7 +1,10 @@
 from __future__ import division
+import math
+import collections
 import operator
 import networkx as nx
 import random
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -18,12 +21,84 @@ class BinRange:
     def __cmp__(self, other):
         return cmp(self.min, other.min)
 
+def div_d(my_dict,constant):
+
+    for i in my_dict:
+        my_dict[i] = float(my_dict[i]/constant)
+
+    return my_dict
 
 def draw_graph(g):
     layout = nx.random_layout(g)
     nx.draw_networkx(g, layout, False, False)
     plt.show()
 
+def draw_empirical_degree_distribution(g):
+
+    degree_sequence = sorted(list([d for n, d in g.degree()]))
+
+    #Find max and min from K
+    min_degree = min(degree_sequence)
+    max_degree = max(degree_sequence)
+    log_min_degree = np.log(min_degree)
+    log_max_degree = np.log(max_degree+1)
+
+    #Find logarithm for all degrees
+    log_degree_sequence = list()
+    for degree in degree_sequence:
+        log_degree_sequence.append(np.log(degree))
+
+    #Divide the interval log_min_degree - log_max_degree in bines
+    bin_divider = (log_max_degree - log_min_degree)/5
+    current_min_limit = log_min_degree
+    log_bins = collections.OrderedDict()
+    log_bin_degree_list = collections.OrderedDict()
+    while(current_min_limit<log_max_degree):
+        log_bins[current_min_limit]=0
+        log_bin_degree_list[current_min_limit] = list()
+        current_min_limit +=bin_divider
+    log_bins[current_min_limit]=0
+    log_bin_degree_list[current_min_limit] = list()
+
+    #Count how many elements of log(k) has into a specific bin
+    for degree in log_degree_sequence:
+       for bin in log_bins:
+           if (degree < bin):
+               log_bins[bin]+=1
+               log_bin_degree_list.get(bin).append(degree)
+               break
+
+
+    #Divide the numbers of element by bin by the graph size
+    probability_list = list()
+    degree_distribution = list()
+    for bin in log_bins:
+            degree_distribution.append(bin)
+            probability_list.append(log_bins[bin]/len(g))
+
+    plt.bar(degree_distribution, probability_list, width=bin_divider, color='r')
+    plt.yticks(np.arange(0, 1, 0.10))
+    plt.xticks(np.arange(log_min_degree+bin_divider, log_max_degree, bin_divider))
+    plt.xlim(log_min_degree+bin_divider, log_max_degree)
+    plt.gca().set_xlabel("Degree")
+    plt.gca().set_ylabel("P(K)")
+    plt.show()
+
+
+
+
+
+
+def draw_empirical_bar(degrees, probabilites,probability_axis,color):
+    plt.bar(degrees, probabilites, width=0.80, color=color)
+    min_degree = min(degrees)
+    max_degree = max(degrees)
+    plt.yticks(np.arange(0,1,0.10))
+    plt.xticks(np.arange(min_degree,max_degree,5))
+    plt.xlim(min_degree, max_degree)
+    plt.gca().set_xlabel("Degree")
+    plt.gca().set_ylabel(probability_axis)
+    plt.show()
 
 
 
@@ -81,9 +156,10 @@ def generate_barbasi_albert(N, seed_nodes, edges_per_new_node):
 
 def main():
     nodes = int(raw_input('Enter the number of nodes '))
-    seed_nodes = int(raw_input('Enter the number of nodes '))
+    seed_nodes = int(raw_input('Enter the number of initial nodes '))
     edges_per_node = int(raw_input('Enter the number of edges per node '))
     graph = generate_barbasi_albert(nodes,seed_nodes,edges_per_node)
-    draw_graph(graph)
+#    draw_graph(graph)
+    draw_empirical_degree_distribution(graph)
 
 main()
