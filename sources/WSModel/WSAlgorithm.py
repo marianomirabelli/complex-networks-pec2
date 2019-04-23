27cb1f0a294ac5e2ad021fd640b32d1f64e92f7a
Mariano <mariano2106@gmail.com>
2019-04-23 01:12:33 -0300

    feature: theoretical probability for watts strogatz

--- sources/WSModel/WSAlgorithm.py
+++ sources/WSModel/WSAlgorithm.py
@@ -1,7 +1,13 @@
 from __future__ import division
 import matplotlib.pyplot as plt
 import networkx as nx
+import collections
 import random
+import math
+import numpy as np
+from scipy.special import comb
+
+
 
 
 
@@ -15,6 +21,54 @@ def draw_graph(g):
     nx.draw_networkx(g, layout, False, False)
     plt.show()
 
+def div_d(my_dict,constant):
+
+    for i in my_dict:
+        my_dict[i] = float(my_dict[i]/constant)
+
+    return my_dict
+
+def draw_empirical_degree_distribution(degree_sequence,n):
+    degree_fec_tuple = collections.Counter(degree_sequence)
+    degree_fec_tuple = div_d(degree_fec_tuple,float(n))
+    degree, probabilities = zip(*degree_fec_tuple.items())
+    plt.bar(degree, probabilities, width=0.80, color='b')
+    min_degree = min(degree)
+    max_degree = max(degree)
+    plt.xticks(np.arange(min_degree, max_degree, 1))
+    plt.xlim(min_degree, max_degree)
+    plt.ylim(0, 1)
+    plt.gca().set_xlabel("Degree")
+    plt.gca().set_ylabel('P(X)')
+    plt.show()
+
+def draw_theoretical_degree_distribution(degree_sequence,n,k,p):
+    half_k = k/2
+    degree_list = list()
+    probability_list = list()
+    for degree in set(degree_sequence):
+        probability = 0
+        index = 1
+        min_value = min(degree-half_k,int(half_k))
+
+        while(index<=min_value):
+            degree_factor = degree - half_k - index
+            probability+= comb(half_k,index) * math.pow(1-p,n) * math.pow(p,half_k-index) * \
+                          (math.pow((p*k)/2,degree_factor))/(math.factorial(degree_factor))*math.exp(-p*half_k)
+            index+=1
+        degree_list.append(degree)
+        probability_list.append(probability)
+
+    plt.bar(degree_list, probability_list, width=0.80, color='y')
+    min_degree = min(degree_sequence)
+    max_degree = max(degree_sequence)
+    plt.xticks(np.arange(min_degree, max_degree, 1))
+    plt.xlim(min_degree, max_degree)
+    plt.gca().set_xlabel("Degree")
+    plt.gca().set_ylabel('P(X)')
+    plt.show()
+
+
 def generate_watts_strogatz(n,k,p):
     if k > n:
         raise ValueError('The k value can not be greater than n')
@@ -22,7 +76,7 @@ def generate_watts_strogatz(n,k,p):
     graph.add_nodes_from(i for i in range(n))
     nodes = graph.nodes()
     half_k = k/2
-    connection_constant = (n - k/2)
+    connection_constant = (n - half_k)
 
     for i in nodes:
 
@@ -50,16 +104,17 @@ def generate_watts_strogatz(n,k,p):
                            graph.add_edge(i, random_node)
                         else:
                             break
-    draw_graph(graph)
-
+    return graph
 def main():
     while(True):
         nodes = int(raw_input('Enter the number of nodes '))
         k = int(raw_input('Enter the number of edges per nodes '))
         p = float(raw_input('Enter the probability'))
-        generate_watts_strogatz(nodes,k,p)
-        nx.watts_strogatz_graph
-
+        graph = generate_watts_strogatz(nodes,k,p)
+        draw_graph(graph)
+        degree_sequence = sorted(list([d for n, d in graph.degree()]))
+        draw_empirical_degree_distribution(degree_sequence,nodes)
+        draw_theoretical_degree_distribution(degree_sequence,nodes,k,p)
 main()
 
 
